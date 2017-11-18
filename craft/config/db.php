9f2419e0cca0c06308cd73c5be625491491e6d50
yulu zhang <yulu@70kft.com>
2017-11-18 10:38:38 +0800

    db ready

---
+++ craft/config/db.php
@@ -0,0 +1,38 @@
+<?php
+
+return array(
+    '*' => array(
+        'tablePrefix' => 'craft',
+    ),
+    'localhost' => array(
+        'server' => 'localhost',
+        'user' => 'root',
+        'password' => '',
+        'database' => 'bbr',
+    ),
+    '192.168.99.100' => array(
+        'server' => 'localhost',
+        'user' => 'root',
+        'password' => '',
+        'database' => 'bbr',
+    ),
+    // Treat *.vagrantshare.com the same as localhost
+    'vagrantshare.com' => array(
+        'server' => 'localhost',
+        'user' => 'root',
+        'password' => '',
+        'database' => 'bbr',
+    ),
+    'accessdomain.com' => array(
+        'server' => 'localhost',
+        'user' => 'the-lab',
+        'password' => '@Z4?zDs%',
+        'database' => 'bbr',
+    ),
+    'eventsbythelab.com' => array(
+        'server' => 'localhost',
+        'user' => 'the-lab',
+        'password' => '@Z4?zDs%',
+        'database' => 'bbr',
+    )
+);
---
+++ craft/config/general.php
@@ -0,0 +1,48 @@
+<?php
+
+return array(
+    '*' => array(
+        'omitScriptNameInUrls' => true,
+    ),
+
+    'localhost' => array(
+        'devMode' => true,
+        'environmentVariables' => array(
+            'env' => 'dev',
+            'fileSystemPath' => '/vagrant/public/',
+            'frontEndAssets' => '/src/',
+            'siteName' => 'bbr',
+        )
+    ),
+
+    'vagrantshare' => array(
+        'devMode' => true,
+        'environmentVariables' => array(
+            'env' => 'dev',
+            'fileSystemPath' => '/vagrant/public/',
+            'frontEndAssets' => '/src/',
+            'siteName' => 'fpma',
+        )
+    ),
+
+    'accessdomain.com' => array(
+        'devMode' => true,
+        'environmentVariables' => array(
+            'env' => 'dev',
+            'fileSystemPath' => '/var/www/the-lab/public/',
+            'frontEndAssets' => '/src/',
+            'siteName' => 'the-lab',
+        )
+    ),
+
+    'eventsbythelab.com' => array(
+        'devMode' => true,
+        'environmentVariables' => array(
+            'env' => 'dev',
+            'fileSystemPath' => '/var/www/the-lab/public/',
+            'frontEndAssets' => '/src/',
+            'siteName' => 'the-lab',
+        )
+    )
+
+);
